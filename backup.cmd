5fbd92a75b0ec0a05cc2411ecc4f671281fca19b
Angel Lobo <angel.lobo.aranguez@gmail.com>
2024-01-03 17:26:05 +0100

    Improve paths with spaces and logs creation

--- backup.cmd
+++ backup.cmd
@@ -8,7 +8,7 @@ IF /I "%PARENT%" == "pwsh" GOTO :ISPOWERSHELL
 endlocal
 
 rem Check if a destination folder is provided as a command-line argument
-if "%1"=="" (
+if %1=="" (
     echo Error: Please provide the destination folder as a command-line argument.
     echo:
     echo Usage: %~nx0 "<Destination folder/path>"
@@ -21,16 +21,16 @@ if "%1"=="" (
 rem Set the working directory to the location of the script
 cd /d "%~dp0"
 
-set LogPath="logs"
-set SourcePathsFile="sources.txt"
-set DestinationPath="%~1"
+set LogPath=logs
+set SourcePathsFile=sources.txt
+set DestinationPath=%~1
 
-set Timestamp="%date:~6,4%-%date:~3,2%-%date:~0,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%"
-set Timestamp="%Timestamp: =0%"
-set Timestamp="%Timestamp:~0,19%"
-set ErrorLogFile="%LogPath%\Backup_Error_Log_%Timestamp%.txt"
+set Timestamp=%date:~6,4%-%date:~3,2%-%date:~0,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
+set Timestamp=%Timestamp: =0%
+set Timestamp=%Timestamp:~0,19%
+set ErrorLogFile=%LogPath%\Backup_Error_Log_%Timestamp%.txt
 
-mkdir "%LogPath%"
+if not exist "%LogPath%" mkdir "%LogPath%"
 
 rem Iterate through each line in the input file
 for /F "usebackq delims=" %%A in (%SourcePathsFile%) do (
@@ -49,7 +49,7 @@ for /F "usebackq delims=" %%A in (%SourcePathsFile%) do (
             set LogFile="%LogPath%\Backup_Log_%Timestamp%_%%~nA.txt"
 
             rem Run the robocopy command for each valid source path
-            robocopy "%%A" "%DestinationPath%" /S /E /XO /NP /LOG:"%LogFile%" /TEE /R:3 /W:5
+            robocopy "%%A" "%DestinationPath%" /S /E /XO /NP /LOG:%LogFile% /TEE /R:3 /W:5
 
             if %errorlevel% gtr 7 (
                 echo Error: Backup operation failed with error code %errorlevel% at %date% %time: =0% >> "%ErrorLogFile%"
